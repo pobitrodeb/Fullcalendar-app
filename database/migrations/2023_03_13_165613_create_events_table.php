ad920db900cd556ed40e38b37a5afc5fcc132d58
Pobitrodeb <pobitrodn@gmail.com>
2023-03-13 23:07:39 +0600

    Successfully done full calender

--- database/migrations/2023_03_13_165613_create_events_table.php
+++ database/migrations/2023_03_13_165613_create_events_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('events', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->date('start');
+            $table->date('end');
             $table->timestamps();
         });
     }
